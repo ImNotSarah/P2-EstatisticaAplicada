fd7717c45f799bec940f7b1eee7d12b0e9b25a37
ImNotSarah <sarahdejesuslima09@gmail.com>
2024-05-21 15:14:55 -0300

    layout dos resultados criado

--- AgrupamentoDiscreto.py
+++ AgrupamentoDiscreto.py
@@ -96,10 +96,63 @@ def calcular_xi():
     cv = round((100 * s) / media, 2)
     x = 150
 
-    CTkLabel(quadro_resultados, text=f"Média = {media}", width=100, height=20, anchor=W, font=("Arial Bold", 16), text_color="black").place(x=x, y=91)
-    CTkLabel(quadro_resultados, text=f"Variância = {S2}", width=100, height=20, anchor=W, font=("Arial Bold", 16), text_color="black").place(x=x, y=111)
-    CTkLabel(quadro_resultados, text=f"Desvio Padrão = {s}", width=100, height=20, anchor=W, font=("Arial Bold", 16), text_color="black").place(x=x, y=131)
-    CTkLabel(quadro_resultados, text=f"Coeficiente Padrão = {cv}", width=100, height=20, anchor=W, font=("Arial Bold", 16), text_color="black").place(x=x, y=151)
+    
+    resultados = CTkLabel(quadro_resultados, text="Resultados", anchor="w", font=("Arial Bold", 32), text_color="#242424")
+    resultados.place(y=30, x=150)
+
+    medidas = CTkLabel(quadro_resultados, text="Medidas de Dispersão", font=("Arial Bold", 18), text_color="#242424")
+    medidas.place(anchor="w", y=80, x=135)
+
+    #quadros
+
+    quadro_n = CTkFrame(quadro_resultados, width=340, height=70, fg_color="#1F6AA5", corner_radius= 15)
+    quadro_n.place(x=55, y=130)
+
+    quadro_media = CTkFrame(quadro_resultados, width=340, height=70, fg_color="#1F6AA5", corner_radius= 15)
+    quadro_media.place(x=55, y=220)
+
+    quadro_variancia = CTkFrame(quadro_resultados, width=340, height=70, fg_color="#1F6AA5", corner_radius= 15)
+    quadro_variancia.place(x=55, y=310)
+
+    quadro_desvio_padrao = CTkFrame(quadro_resultados,width=340, height=70, fg_color="#1F6AA5", corner_radius= 15)
+    quadro_desvio_padrao.place(x=55, y=400)
+
+    quadro_coeficiente_padrao = CTkFrame(quadro_resultados, width=340, height=70, fg_color="#1F6AA5", corner_radius= 15)
+    quadro_coeficiente_padrao.place(x=55, y=490)
+
+    #Resultados
+
+    valor_n = CTkLabel(quadro_n, text=f"{N:.2f}", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    valor_n.place(x=260, y=30)
+
+    valor_media = CTkLabel(quadro_media, text=f"{media:.2f}", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    valor_media.place(x=260, y=30)
+
+    valor_variancia = CTkLabel(quadro_variancia, text=f"{S2:.2f}", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    valor_variancia.place(x=260, y=30)
+
+    valor_desvio_padrao = CTkLabel(quadro_desvio_padrao, text=f"{s:.2f}", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    valor_desvio_padrao.place(x=260, y=30)
+
+    valor_coeficiente_padrao = CTkLabel(quadro_coeficiente_padrao, text=f"{cv:.2f}%", width=70, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    valor_coeficiente_padrao.place(x=260, y=30)
+
+    #Textos
+
+    txt_N = CTkLabel(quadro_n, text="N", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    txt_N.place(x=15, y=10)
+
+    txt_media = CTkLabel(quadro_media, text="Média", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    txt_media.place(x=15, y=10)
+
+    txt_variacia = CTkLabel(quadro_variancia, text="Variância", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    txt_variacia.place(x=15, y=10)
+
+    txt_desvio_padrao = CTkLabel(quadro_desvio_padrao, text="Desvio Padrão", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    txt_desvio_padrao.place(x=15, y=10)
+
+    txt_coeficiente_padrao = CTkLabel(quadro_coeficiente_padrao, text="Coeficiênte Padrão", width=100, height=20, anchor=W, font=("Arial Bold", 22), text_color="white")
+    txt_coeficiente_padrao.place(x=15, y=10)
 
 def coletar_quantidade():
     return int(entrada_quant_xi.get())
@@ -107,25 +160,25 @@ def coletar_quantidade():
 l_x = 50
 l_y = 130
 
-txt_quantidade = CTkLabel(estatistica, text="Digite quantos Dados(Xi) a amostra contém", font=("Arial Bold", 18))
-txt_quantidade.place(x=l_x, y=l_y)
-
-entrada_quant_xi = CTkEntry(estatistica, width=240, height=25, font=("Arial Bold", 14), border_width=1, corner_radius=0)
-entrada_quant_xi.place(x=l_x, y=170)
-
 titulo = CTkLabel(estatistica, text="Estatística Aplicada", anchor="w", font=("Arial Bold", 32))
 titulo.place(anchor="w", y=50, x=85)
 
 orientacao = CTkLabel(estatistica, text="Agrupamento Discreto", font=("Arial Bold", 18))
 orientacao.place(anchor="w", y=85, x=130)
 
-quadro_dados = CTkScrollableFrame(estatistica, width=185, height=240, fg_color="#242424")
+txt_quantidade = CTkLabel(estatistica, text="Digite quantos Dados(Xi) a amostra contém", font=("Arial Bold", 18))
+txt_quantidade.place(x=l_x, y=l_y)
 
-quadro_resultados = CTkFrame(estatistica, width=450, height=600, fg_color="white")
-quadro_resultados.place(x=450, y=0)
+entrada_quant_xi = CTkEntry(estatistica, width=240, height=25, font=("Arial Bold", 14), border_width=1, corner_radius=0)
+entrada_quant_xi.place(x=l_x, y=170)
 
 quadro_ok = CTkFrame(estatistica, width=150, height=35, fg_color="#242424")
 quadro_ok.place(x=300, y=165)
+
+quadro_dados = CTkScrollableFrame(estatistica, width=185, height=240, fg_color="#242424")
+
+quadro_resultados = CTkFrame(estatistica, width=450, height=600, fg_color="#F9F9FA", corner_radius=0)
+quadro_resultados.place(x=450, y=0)
 
 quadro_calcular = CTkFrame(estatistica, width=390, height=50, fg_color="#242424")
 quadro_calcular.place(x=30, y=520)
